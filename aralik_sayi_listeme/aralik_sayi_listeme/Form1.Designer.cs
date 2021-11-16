8458995d2019a63db8b8a001b63fd640d2db506d
ridvansunger <rdvansunger@gmail.com>
2021-11-16 22:20:28 +0300

    degerden degere

---
+++ aralik_sayi_listeme/aralik_sayi_listeme/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace aralik_sayi_listeme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnListele = new System.Windows.Forms.Button();
+            this.txtSayi1 = new System.Windows.Forms.TextBox();
+            this.txtSayi2 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCiftYazdir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sayi1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sayi2";
+            // 
+            // btnListele
+            // 
+            this.btnListele.Location = new System.Drawing.Point(56, 164);
+            this.btnListele.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnListele.Name = "btnListele";
+            this.btnListele.Size = new System.Drawing.Size(168, 52);
+            this.btnListele.TabIndex = 2;
+            this.btnListele.Text = "Listele";
+            this.btnListele.UseVisualStyleBackColor = true;
+            this.btnListele.Click += new System.EventHandler(this.btnListele_Click);
+            // 
+            // txtSayi1
+            // 
+            this.txtSayi1.Location = new System.Drawing.Point(111, 68);
+            this.txtSayi1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtSayi1.Name = "txtSayi1";
+            this.txtSayi1.Size = new System.Drawing.Size(112, 27);
+            this.txtSayi1.TabIndex = 3;
+            // 
+            // txtSayi2
+            // 
+            this.txtSayi2.Location = new System.Drawing.Point(111, 115);
+            this.txtSayi2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtSayi2.Name = "txtSayi2";
+            this.txtSayi2.Size = new System.Drawing.Size(112, 27);
+            this.txtSayi2.TabIndex = 4;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(266, 68);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(201, 244);
+            this.listBox1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(72, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(220, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "İstediğiniz Sayı Aralığını Giriniz";
+            // 
+            // btnCiftYazdir
+            // 
+            this.btnCiftYazdir.Location = new System.Drawing.Point(61, 246);
+            this.btnCiftYazdir.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnCiftYazdir.Name = "btnCiftYazdir";
+            this.btnCiftYazdir.Size = new System.Drawing.Size(163, 52);
+            this.btnCiftYazdir.TabIndex = 7;
+            this.btnCiftYazdir.Text = "Çift Sayıları Listele";
+            this.btnCiftYazdir.UseVisualStyleBackColor = true;
+            this.btnCiftYazdir.Click += new System.EventHandler(this.btnCiftYazdir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 382);
+            this.Controls.Add(this.btnCiftYazdir);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtSayi2);
+            this.Controls.Add(this.txtSayi1);
+            this.Controls.Add(this.btnListele);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnListele;
+        private System.Windows.Forms.TextBox txtSayi1;
+        private System.Windows.Forms.TextBox txtSayi2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCiftYazdir;
+    }
+}
+
---
+++ aralik_sayi_listeme/aralik_sayi_listeme/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aralik_sayi_listeme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            int sayi1 = Convert.ToInt32(txtSayi1.Text);
+            int sayi2 = Convert.ToInt32(txtSayi2.Text);
+
+            for (int i = sayi1; i <= sayi2; i++)
+            {
+                listBox1.Items.Add(i);
+            }
+        }
+
+        private void btnCiftYazdir_Click(object sender, EventArgs e)
+        {
+
+            listBox1.Items.Clear();
+
+            int sayi3 = Convert.ToInt32(txtSayi1.Text);
+            int sayi4 = Convert.ToInt32(txtSayi2.Text);
+
+            string cift = "Çift Sayılar:";
+            listBox1.Items.Add(cift);
+
+            for (int i = sayi3; i <=sayi4; i++)
+            {
+                
+
+                if (i % 2 == 0)
+                {
+                    
+                    listBox1.Items.Add(i);
+                }
+
+            }
+            
+               
+            
+        }
+    }
+}
